a77cdbc878a7f8b8f5ce4f02efb9af965166643c
sina-mokhtari <sina.mokhtari2000@gmail.com>
2022-06-05 23:10:39 +0430

    four threads

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int findFirstOccur(char *str, char *wrd) {
     char word[25], string[131072];
@@ -42,20 +45,56 @@ int findFirstOccur(char *str, char *wrd) {
 
 int mails = 0;
 pthread_mutex_t mutex;
+pthread_t threads[4];
+char *string;
+int length;
 
-void *routine(void *string) {
-    //  pthread_mutex_lock(&mutex);
-    // mails++;
-    //    pthread_mutex_unlock(&mutex);
+void *routine1(void *args) {
+    // printf("shit");
+    char *word = "abc";
     int *line = malloc(sizeof(int));
-    *line = findFirstOccur((char *)string, "abc");
-    return (void *)line;
+    char quarterStr[1000];
+
+    int threadNumber = 5;
+    if (pthread_self() == threads[0]) threadNumber = 1;
+    if (pthread_self() == threads[1]) threadNumber = 2;
+    if (pthread_self() == threads[2]) threadNumber = 3;
+    if (pthread_self() == threads[3]) threadNumber = 4;
+
+    int quarterSize = length / 4;
+    switch (threadNumber) {
+        case 1:
+            strncpy(quarterStr, string, quarterSize);
+            quarterStr[quarterSize] = '\0';
+            break;
+        case 2:
+            strncpy(quarterStr, string + quarterSize, quarterSize);
+            quarterStr[quarterSize] = '\0';
+            break;
+        case 3:
+            strncpy(quarterStr, string + quarterSize * 2, quarterSize);
+            quarterStr[quarterSize] = '\0';
+            break;
+        case 4:
+            strncpy(quarterStr, string + quarterSize * 3,
+                    length - quarterSize * 3);
+            quarterStr[length - quarterSize * 3] = '\0';
+            break;
+        default:
+            printf("shit");
+            return NULL;
+            break;
+    }
+    *line = findFirstOccur(quarterStr, word);
+    if (*line != 0) {
+        printf("%s word found on line %d in thread %d\n", word, *line,
+               threadNumber);
+    }
+    // return (void *)line;
 }
 
 int main(int argc, char *argv[]) {
     FILE *f = fopen("text.txt", "r");
-    int length;
-    char *string;
     if (f) {
         fseek(f, 0, SEEK_END);
         length = ftell(f);
@@ -66,25 +105,27 @@ int main(int argc, char *argv[]) {
         }
         fclose(f);
     }
+    // int quarterSize = length / 4;
 
-    pthread_t threads[4];
     int *returnValue;
 
     for (int i = 0; i < 4; i++)
-        if (pthread_create(&threads[i], NULL, &routine, string) != 0) return 1;
+        if (pthread_create(&threads[i], NULL, &routine1, NULL) != 0) return 1;
+
+    // for (int i = 0; i < 4; i++) printf("thread:%ld\n", threads[i]);
 
     // pthread_t p1, p2, p3, p4;
     // pthread_mutex_init(&mutex, NULL);
 
     for (int i = 0; i < 4; i++)
-        if (pthread_join(threads[i], (void **)&returnValue) != 0)
-            perror("thread fail!");
+        if (pthread_join(threads[i], NULL) != 0) perror("thread fail!");
 
-    printf("%d\n", *returnValue);
-    // if (pthread_join(p4, NULL) != 0) {
-    //     return 8;
-    // }
-    // pthread_mutex_destroy(&mutex);
-    // printf("Number of mails: %d\n", mails);
+    // printf("%d\n", *returnValue);
+    // printf("thread id: %ld\n", syscall(__NR_gettid));
+    //  if (pthread_join(p4, NULL) != 0) {
+    //      return 8;
+    //  }
+    //  pthread_mutex_destroy(&mutex);
+    //  printf("Number of mails: %d\n", mails);
     return 0;
 }
